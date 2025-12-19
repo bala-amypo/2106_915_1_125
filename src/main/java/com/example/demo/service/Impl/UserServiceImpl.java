ffbf56f7632ead7cb3f3566089a40c27c36b3af2
bala-amypo <bala@amypo.in>
2025-12-19 06:12:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.User;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User registerUser(User user) {
+
+        if (user.getUsername() == null || user.getUsername().isEmpty()) {
+            throw new BadRequestException("Username required");
+        }
+
+        userRepository.findByUsernameIgnoreCase(user.getUsername())
+                .ifPresent(u -> {
+                    throw new BadRequestException("Username already exists");
+                });
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
