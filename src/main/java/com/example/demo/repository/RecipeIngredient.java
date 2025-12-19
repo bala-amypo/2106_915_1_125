6854edfa5610cf1645d8cc39a42fb2894a3779f9
bala-amypo <bala@amypo.in>
2025-12-19 05:36:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/RecipeIngredient.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.RecipeIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
+
+    List<RecipeIngredient> findByMenuItemId(Long menuItemId);
+
+    boolean existsByMenuItemId(Long menuItemId);
+
+    @Query("SELECT SUM(r.quantityRequired) FROM RecipeIngredient r WHERE r.ingredient.id = :ingredientId")
+    Double getTotalQuantityByIngredientId(@Param("ingredientId") Long ingredientId);
+}
