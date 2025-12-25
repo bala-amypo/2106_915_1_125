e8855d06ad8612ee176013827a76d92d30e8434e
bala-amypo <bala@amypo.in>
2025-12-25 06:22:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,32 +1,21 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.RegisterRequest;
-import com.example.demo.entity.User;
+import com.example.demo.dto.LoginRequest;
 import com.example.demo.security.JwtTokenProvider;
-import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthController(UserService userService,
-                          JwtTokenProvider jwtTokenProvider) {
-        this.userService = userService;
+    public AuthController(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    @PostMapping("/register")
-    public User register(RegisterRequest request) {
-        return userService.register(request);
-    }
-
     @PostMapping("/login")
-    public String login(AuthRequest request) {
+    public String login(@RequestBody LoginRequest request) {
         return jwtTokenProvider.generateToken(request.getUsername());
     }
 }
