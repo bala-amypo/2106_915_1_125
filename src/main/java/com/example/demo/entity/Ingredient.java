c7a65bd64bb0ffce541e1469f502fb6e29933348
bala-amypo <bala@amypo.in>
2025-12-26 15:57:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/Ingredient.java
+++ src/main/java/com/example/demo/entity/Ingredient.java
@@ -1,22 +1,21 @@
 package com.example.demo.entity;
 
-import java.math.BigDecimal;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ingredient {
-    private Long id;
-    private String name;
-    private String unit;
-    private BigDecimal costPerUnit;
-    private Boolean active = true;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getUnit() { return unit; }
-    public void setUnit(String unit) { this.unit = unit; }
-    public BigDecimal getCostPerUnit() { return costPerUnit; }
-    public void setCostPerUnit(BigDecimal costPerUnit) { this.costPerUnit = costPerUnit; }
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private boolean active;
 }
