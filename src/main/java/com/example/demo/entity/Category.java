1f86aa24103bd29192c212ac74d08d6933c7be31
bala-amypo <bala@amypo.in>
2025-12-26 15:51:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/Category.java
+++ src/main/java/com/example/demo/entity/Category.java
@@ -1,20 +1,20 @@
 package com.example.demo.entity;
 
-import java.util.HashSet;
-import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Category {
-    private Long id;
-    private String name;
-    private Boolean active = true;
-    private Set<MenuItem> menuItems = new HashSet<>();
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
-    public Set<MenuItem> getMenuItems() { return menuItems; }
-    public void setMenuItems(Set<MenuItem> menuItems) { this.menuItems = menuItems; }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private boolean active;
+
+    // getters and setters
 }
