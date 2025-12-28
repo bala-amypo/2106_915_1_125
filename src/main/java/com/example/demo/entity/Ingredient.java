e4ed25cfa3eab9fbe95ad734b7cc066af6116f76
bala-amypo <bala@amypo.in>
2025-12-28 07:55:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/Ingredient.java
+++ src/main/java/com/example/demo/entity/Ingredient.java
@@ -10,35 +10,9 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String name;
 
     private boolean active = true;
 
-    public Ingredient() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
+    // getters & setters
 }
--- src/main/java/com/example/demo/entity/RecipeIngredient.java
+++ src/main/java/com/example/demo/entity/RecipeIngredient.java
@@ -10,36 +10,11 @@ public class RecipeIngredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
+    @ManyToOne
     @JoinColumn(name = "ingredient_id")
     private Ingredient ingredient;
 
     private Double quantity;
 
-    public RecipeIngredient() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Ingredient getIngredient() {
-        return ingredient;
-    }
-
-    public void setIngredient(Ingredient ingredient) {
-        this.ingredient = ingredient;
-    }
-
-    public Double getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Double quantity) {
-        this.quantity = quantity;
-    }
+    // getters & setters
 }
